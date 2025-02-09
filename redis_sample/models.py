c52c07daa0b88ab9d1d44e79aa4c753850b7bed9
Muneeb Shahid <muneeb706@gmail.com>
2025-02-09 00:36:14 -0800

    feat: Add historical tracking to Event and Ticket models using django-simple-history

--- redis_sample/models.py
+++ redis_sample/models.py
@@ -1,12 +1,13 @@
 from django.db import models
-
+from simple_history.models import HistoricalRecords
 
 class Event(models.Model):
     name = models.CharField(max_length=200)
     total_tickets = models.IntegerField()
     available_tickets = models.IntegerField()
-
+    history = HistoricalRecords()
 
 class Ticket(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     customer_username = models.CharField(max_length=200)  # Simplified for this example
+    history = HistoricalRecords()
