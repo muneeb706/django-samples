b98aa6c713cc3b56fceb5c91776f2bdc7fef0a64
Muneeb Shahid <muneeb706@gmail.com>
2025-02-09 00:35:52 -0800

    refactor: Comment out Redis initialization logic in RedisSampleConfig

--- redis_sample/apps.py
+++ redis_sample/apps.py
@@ -5,26 +5,26 @@ class RedisSampleConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "redis_sample"
 
-    def ready(self):
-        """
-        Makes sure the Redis cache is consistent with the database,
-        initializes the ticket counts in Redis
-        when the application starts.
-        """
-        from django_redis import get_redis_connection
+    # def ready(self):
+    #     """
+    #     Makes sure the Redis cache is consistent with the database,
+    #     initializes the ticket counts in Redis
+    #     when the application starts.
+    #     """
+    #     from django_redis import get_redis_connection
 
-        from .models import Event
+    #     from .models import Event
 
-        def initialize_redis(sender, **kwargs):
-            redis_conn = get_redis_connection("default")
-            events = Event.objects.all()
-            for event in events:
-                is_set_succcessful = redis_conn.set(
-                    f"event:{event.id}:available_tickets", event.available_tickets
-                )
-                if is_set_succcessful:
-                    print("Redis reconcilliation operation was successful.")
-                else:
-                    print("Redis reconcilliation operation failed.")
+    #     def initialize_redis(sender, **kwargs):
+    #         redis_conn = get_redis_connection("default")
+    #         events = Event.objects.all()
+    #         for event in events:
+    #             is_set_succcessful = redis_conn.set(
+    #                 f"event:{event.id}:available_tickets", event.available_tickets
+    #             )
+    #             if is_set_succcessful:
+    #                 print("Redis reconcilliation operation was successful.")
+    #             else:
+    #                 print("Redis reconcilliation operation failed.")
 
-        initialize_redis(sender=self)
+    #     initialize_redis(sender=self)
